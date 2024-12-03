ca9ad60d25c01fc0619e00436307a9aa29b5370e
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2024-12-03 01:44:01 -0500

    Day 2: Part 2

--- src/main/kotlin/com/lucaspowered/aoc2024/Day2.kt
+++ src/main/kotlin/com/lucaspowered/aoc2024/Day2.kt
@@ -7,14 +7,12 @@ fun main() = day2()
 
 fun day2() {
     val input = Files.readAllLines(Path.of("inputs/Day2.txt")).map { i -> i.split(" ").map { it.toInt() } }
-    //println(input)
+
     // Part 1
-    println(input.fold(0) { acc, report ->
-        if ((report.isIncreasing() || report.isDecreasing()) && report.valsWithinTolerance(
-                1..3
-            )
-        ) acc + 1 else acc
-    })
+    println("Part 1: ${input.fold(0) { acc, report -> if (isSafe(report)) acc + 1 else acc }}")
+
+    // Part 2
+    println("Part 2: ${input.fold(0) { acc, report -> if (isSafe(report, dampener = true)) acc + 1 else acc }}")
 }
 
 private fun List<Int>.isIncreasing(): Boolean {
@@ -35,7 +33,22 @@ private fun List<Int>.isDecreasing(): Boolean {
     return true
 }
 
-private fun List<Int>.valsWithinTolerance(range: IntRange): Boolean {
-    this.forEachIndexed { index, _ -> if (index < this.lastIndex) if (!range.contains(abs(this[index + 1] - this[index]))) return false }
-    return true
+private fun List<Int>.exclude(index: Int): List<Int> {
+    if (index == -1) return this
+    return this.filterIndexed { idx, _ -> idx != index }
+}
+
+private fun isSafe(list: List<Int>, range: IntRange = 1..3, counter: Int = -1, dampener: Boolean = false): Boolean {
+    val l = list
+    if ((l.exclude(counter).isIncreasing() || l.exclude(counter).isDecreasing()) && l.exclude(counter)
+            .filterIndexed { idx, _ ->
+                (idx < l.exclude(counter).lastIndex && !range.contains(
+                    abs(
+                        l.exclude(counter)[idx] - l.exclude(counter)[idx + 1]
+                    )
+                ))
+            }.isEmpty()
+    ) return true
+    if (!dampener) return false
+    return if (counter == l.lastIndex) false else isSafe(list = l, counter = counter + 1, dampener = dampener)
 }
