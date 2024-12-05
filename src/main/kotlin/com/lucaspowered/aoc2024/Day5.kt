84295bc14b55e5db917f656c4988b0101fc8dfce
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2024-12-05 13:43:08 -0500

    Day 5: Part 2

--- src/main/kotlin/com/lucaspowered/aoc2024/Day5.kt
+++ src/main/kotlin/com/lucaspowered/aoc2024/Day5.kt
@@ -1,38 +1,79 @@
 package com.lucaspowered.aoc2024
 
-import java.io.Serializable
 import java.nio.charset.Charset
 import java.nio.file.*
 import kotlin.io.path.Path
 
 fun main() = day5()
 private val input = Files.readAllBytes(Path("inputs/Day5.txt")).toString(Charset.defaultCharset())
-//private val input = "7|53\n 97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47\n"
-private val order = Regex("\\d+\\|\\d+").findAll(input).map {Pair(it.value.split('|')[0], it.value.split('|')[1])}
-private val updates = input.split('\n').filter {it.contains(',')}.map {it.split(',')}
+private val order = Regex("\\d+\\|\\d+").findAll(input).map { Pair(it.value.split('|')[0], it.value.split('|')[1]) }
+private val updates = input.split('\n').filter { it.contains(',') }.map { it.split(',').toTypedArray() }.toTypedArray()
 
 fun day5() {
+    // Part 1
     val good = updates.getGoodUpdates()
-    println(good.fold(0) {acc, e -> acc + e[e.lastIndex / 2].toInt()})
+    println("Part 1: ${good.fold(0) { acc, e -> acc + e[e.lastIndex / 2].toInt() }}")
+
+    // Part 2
+    val corrected = updates.getBadUpdates()
+    corrected.correctUpdates()
+    println("Part 2: ${corrected.fold(0) { acc, e -> acc + e[e.lastIndex / 2].toInt() }}")
 }
 
-private fun List<List<String>>.getBadUpdates(): List<List<String>> {
-    val ruleBreakers = mutableListOf<List<String>>()
-    for (i in this){
+private fun Array<Array<String>>.getBadUpdates(): Array<Array<String>> {
+    val ruleBreakers = mutableListOf<Array<String>>()
+    for (i in this) {
         i.forEach { j ->
-            val forbiddenBefore = order.filter { j == it.first }.map {it.second}
-            forbiddenBefore.forEach {
-                    k ->
-                if (i.indexOf(k) >= 0  && i.indexOf(j) >= 0 && i.indexOf(k) < i.indexOf(j)){
+            val forbiddenBefore = order.filter { j == it.first }.map { it.second }
+            forbiddenBefore.forEach { k ->
+                if (i.indexOf(k) >= 0 && i.indexOf(j) >= 0 && i.indexOf(k) < i.indexOf(j)) {
                     ruleBreakers.add(i)
                 }
             }
         }
     }
-    return ruleBreakers.toSet().toList()
+    return ruleBreakers.toSet().toTypedArray()
 }
 
-private fun List<List<String>>.getGoodUpdates(): List<List<String>> {
+private fun Array<Array<String>>.correctUpdates() {
+    this.forEach { i ->
+        while (i.isBadUpdate()) {
+            i.forEach { x ->
+                val forbiddenBefore = order.filter { x == it.first }.map { it.second }
+                forbiddenBefore.forEach { y ->
+                    if (i.indexOf(y) >= 0 && i.indexOf(x) >= 0 && i.indexOf(y) < i.indexOf(x)) {
+                        i.swap(x, y)
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun Array<Array<String>>.getGoodUpdates(): Array<Array<String>> {
     val bad = this.getBadUpdates()
-    return updates.filterNot {bad.contains(it)}
+    return updates.filterNot { bad.contains(it) }.toTypedArray()
+}
+
+private fun Array<String>.isBadUpdate(): Boolean {
+    this.forEach { x ->
+        val forbiddenBefore = order.filter { x == it.first }.map { it.second }
+        forbiddenBefore.forEach { y ->
+            if (this.indexOf(y) >= 0 && this.indexOf(x) >= 0 && this.indexOf(y) < this.indexOf(x)) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+private fun Array<String>.isGoodUpdate(): Boolean {
+    return !this.isBadUpdate()
+}
+
+private fun <T> Array<T>.swap(x: T, y: T) {
+    val xidx = this.indexOf(x)
+    val yidx = this.indexOf(y)
+    this[xidx] = y
+    this[yidx] = x
 }
